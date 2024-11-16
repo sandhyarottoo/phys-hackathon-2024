430cea736d58c3b7f0dd50f82756b69d475c2bdd
sandhyarottoo <sandhya.rottoo@gmail.com>
2024-11-16 16:25:23 -0500

    grhusdf

--- GLOBVAR.py
+++ GLOBVAR.py
@@ -1,6 +1,11 @@
+import pygame
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 NBOX_X = 8
 NBOX_Y = 6
 BOX_WIDTH = SCREEN_WIDTH // NBOX_X
 BOX_HEIGHT = SCREEN_HEIGHT // NBOX_Y
+
+
+canon_png = pygame.image.load('canon.png')
